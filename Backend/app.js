87c4591a784d3d05b3f704699bb01377503e3e88
chirakulal <119620590+chirakulal@users.noreply.github.com>
2024-08-22 23:24:39 +0530

    added authentication

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,15 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+const authRoute = require("./routes/auth")
 
 
 module.exports=app;
+
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use("/api/v1/auth", authRoute )
+
